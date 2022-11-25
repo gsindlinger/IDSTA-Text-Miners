a53bd3f964d4f673b9883b1de3197dadc8492023
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2022-11-25 09:41:50 +0100

    Added ArtistCollection.py and all procedures to retrieve artist data and store them to csv, as well to read it back to collection

---
+++ Project/ArtistCollection.py
@@ -0,0 +1,46 @@
+import csv
+from typing import Set, Dict
+
+
+class ArtistCollection:
+    def __init__(self):
+        self.__list: Dict[str, Set[int]] = {}
+
+    @property
+    def list(self):
+        return self.__list
+
+    def append_item(self, name: str, year: int) -> None:
+        if self.is_existing_artist(name):
+            self.get_year_set(name).add(year)
+        else:
+            self.__list.update({name: {year}})
+
+    def is_existing_artist(self, name: str) -> bool:
+        if name in self.__list:
+            return True
+        else:
+            return False
+
+    def get_year_set(self, name: str) -> Set[int]:
+        try:
+            return self.__list[name]
+        except KeyError:
+            print("Artist name doesn't exist!")
+
+    def write_to_csv(self, filename: str) -> None:
+        with open(filename, "w", newline='', encoding='utf-8') as outfile:
+            writer = csv.writer(outfile)
+            for name, years in self.__list.items():
+                row = [name]
+                row.extend(list(map(str, years)))
+                writer.writerow(row)
+
+    def read_csv_to_object(self, filename: str):
+        self.__list.clear()
+        with open(filename) as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                name = row.pop(0)
+                years = set([int(y) for y in row])
+                self.__list.update({name: years})
---
+++ Project/Readme.md
@@ -0,0 +1,18 @@
+# Artist generation
+
+The artists for scraping via Genius were determined using the Spotify API. The playlists "Deutschrap xxxx" were used, where "xxxx" stands for a year in the period 1998-2022. For each year, either only the official Spotify playlist is selected or multiple playlists from different creators.
+
+For the first alternative, the file [data/artist_data_single.csv](data/artist_data_single.csv) was created as an example, for the second alternative the file [data/artist_data.csv](data/artist_data.csv). The generated CSV files can be read via read_csv_to_object method in the [ArtistCollection.py](ArtistCollection.py) class.
+
+## The procedure
+For each playlist requested with the search query "Deutschrap xxxx", all artists are extracted and then added to the ArtistCollection. Thereby it is checked if the artist already exists: If yes, only one entry is added for this artist for the concerned year, so that each artist appears only once in the ArtistCollection.
+
+
+## Getting started
+1. if not already done, create Spotify API credentials online.
+2. create a file called ``config.py`` and add the credentials with the names ``SPOTIFY_CLIENT_ID`` and
+``SPOTIFY_CLIENT_SECRET``.
+3. install requirements: ``` pip install -r requirements.txt```
+4. modify the code in file ``main.py`` according to the functionality you want.
+5. run ``python .\main.py```
+
