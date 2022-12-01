afe7ed891ea7ce22ac2011b95f062ff82d78475b
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2022-12-01 17:00:38 +0100

    renaming GeniusScraping class

--- Project/Lyrics_Scraping/Readme.md
+++ Project/Lyrics_Scraping/Readme.md
@@ -1,2 +1,6 @@
 # Lyrics Scraping
 
+## Getting started
+1. if not already done, create Genuis API credentials online.
+2. create a file called ``config.py`` and add the access token with the name ``ACCESS_TOKEN``
+3. 
--- Project/Lyrics_Scraping/main.py
+++ Project/Lyrics_Scraping/main.py
@@ -1,10 +1,10 @@
 from Artist_Generation.ArtistCollection import ArtistCollection, read_csv_to_artist_collection
-from Lyrics_Scraping.GeniusAPI import GeniusAPI
+from Lyrics_Scraping.GeniusScraping import GeniusScraping
 
 if __name__ == '__main__':
     artist_collection = read_csv_to_artist_collection("../Artist_Generation/data/artist_data_single.csv")
 
-    genius_api = GeniusAPI()
+    genius_api = GeniusScraping()
     print(artist_collection.list.keys())
     genius_api.request_artist_info(list(artist_collection.list.keys())[0])
     print("Reading artist collection done!")
