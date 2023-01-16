8cc576b502379061a729b751755c80b332f873c1
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2023-01-16 14:25:26 +0100

    update main.py file

--- Project/Pipeline/preprocessing/main.py
+++ Project/Pipeline/preprocessing/main.py
@@ -3,15 +3,16 @@ from Pipeline.Lyrics_Scraping.GeniusLyricsExtraction import GeniusSongsDict, Gen
 from Pipeline.preprocessing.CategoryDictionary import CategoryDictionary, read_from_json
 
 if __name__ == '__main__':
-    '''
     w2v_dictionary = CategoryDictionary()
     print('model loaded')
     w2v_dictionary.build_dictionary_model()
     print('dictionary computed')
 
+    '''
     songs_dict: GeniusSongs = GeniusLyricsExtraction.read_song_list_from_json_dict("only_german_lyrics.json")
     songs_dict = songs_dict.song_list[:10]
 
-    '''
+    
     w2v_dictionary_loaded = read_from_json("data/categories.json")
+    '''
     print("done")
