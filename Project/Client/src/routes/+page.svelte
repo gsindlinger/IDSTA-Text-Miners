39f703987a6379cefc9b94e1d4974c6e4e5ec882
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2023-01-30 08:44:33 +0100

    first page frontend

--- Project/Client/src/routes/+page.svelte
+++ Project/Client/src/routes/+page.svelte
@@ -1,59 +1,34 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import TitlePage from "$lib/components/TitlePage.svelte";
+	import MotivationPage from "$lib/components/MotivationPage.svelte";
+	import Chevron from "$lib/components/Chevron.svelte";
+	import Scrolly from "$lib/components/Scrolly.svelte";
+
+	let scrollStep;
+	$: console.log(scrollStep)
 </script>
 
+
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>German Rap Analysis</title>
+	<meta name="description" content="German Rap Analysis" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div class="main-container">
+	<section>
+		<TitlePage/>
+		<Scrolly bind:value={scrollStep}>
+			<MotivationPage active={scrollStep === 0}/>
+			<MotivationPage active={scrollStep === 1}/>
+		</Scrolly>
+	</section>
+</div>
+<Chevron/>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
+	.main-container {
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
+
