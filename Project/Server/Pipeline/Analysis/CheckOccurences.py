6614e78db5bbac2c1908e8a0e7cce131702475aa
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2023-02-09 22:10:58 +0100

    initial commit

--- Project/Server/Pipeline/Analysis/CheckOccurences.py
+++ Project/Server/Pipeline/Analysis/CheckOccurences.py
@@ -1,5 +1,5 @@
 import concurrent
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import Tuple, Dict, List
 
 import pandas as pd
@@ -10,7 +10,7 @@ import Util
 from tqdm import *
 
 categories: CategoryDictionary = read_from_json("data/categories.json")
-
+lemmatized_categories: Dict[str, List[str]] = defaultdict()
 
 
 def check_occurences_songs(songs: GeniusSongs) -> None:
@@ -22,13 +22,29 @@ def check_occurences_songs(songs: GeniusSongs) -> None:
     @param songs:
     @return:
     """
+
+    print("start lemmatizing categories")
+    for category, values in tqdm(categories.categories.items()):
+        # lemmatized_categories[category] = [term.lower() for term in values]
+        lemmatized_categories[category] = [lemmatize_filtered(term).lower().strip() for term in values if term]
+
     print("start checking occurences of categories")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        list(tqdm(executor.map(check_occurences_song_already_lemmatized, songs.song_list), total=len(songs.song_list)))
+
+    for song in tqdm(songs.song_list):
+        song.matched_categories = None
+        check_occurences_song_already_lemmatized(song)
+
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    #   list(tqdm(executor.map(check_occurences_song_already_lemmatized, songs.song_list), total=len(songs.song_list)))
+
+
+def lemmatize_filtered(term: str) -> str:
+    lemmatize_helper = Util.lemmatize(term)[0]
+    return lemmatize_helper if lemmatize_helper not in ["", "--"] else term
 
 
 def check_occurences_song_already_lemmatized(song: Song) -> None:
-    for category, category_values in categories.categories.items():
+    for category, category_values in lemmatized_categories.items():
         preprocessed_lyrics = "".join(song.processed_lyrics_text)
         '''
         preprocessed_lyrics = Util.remove_stopwords_and_punctuation(preprocessed_lyrics)
@@ -46,7 +62,7 @@ def check_occurences_song(song: Song) -> None:
     preprocessed_lyrics, word_mapping = Util.lemmatize(preprocessed_lyrics)
     word_mapping = Util.reverse_dict(word_mapping)
 
-    for category, category_values in categories.categories.items():
+    for category, category_values in lemmatized_categories.items():
         check_against_category(preprocessed_lyrics, word_mapping, song, category, category_values)
 
 
@@ -63,29 +79,52 @@ def check_against_category(preprocessed_lyrics_temp: str, word_mapping: Dict[str
                 song_temp.matched_categories[category] = [(word, count, word_mapping[word])]
 
 
-def check_against_category_without_mapping(preprocessed_lyrics: str, song_temp: Song, category: str, category_values: Tuple[str, str]):
+def check_against_category_without_mapping(preprocessed_lyrics: str,
+                                           song_temp: Song, category: str, category_values: List[str]):
+
     if song_temp.matched_categories is None:
         song_temp.matched_categories = {}
     counted_words = Counter(preprocessed_lyrics.split())
+    matched_categories = {}
     for word, count in counted_words.most_common():
-        if word in list(map(lambda x: x[1], category_values)):
-            if category in song_temp.matched_categories:
-                song_temp.matched_categories[category].append((word, count, ""))
-            else:
-                song_temp.matched_categories[category] = [(word, count, "")]
+        for category_value in category_values:
+            # also allow substrings to be matched
+            # if one of the words in the lyrics starts with or ends with a word of the categories then consider them always
+            # if a word of the lyrics of a substring is part of a word of the category value only consider them if
+            # this word is at least 75% equal to the given category value
+            bool1 = word in category_value and len(word) >= 0.75 * len(category_value)
+            bool2 = word.startswith(category_value) or word.endswith(category_value)
+            if bool1 or bool2:
+                matched_categories[word] = count
+
+    if matched_categories:
+        song_temp.matched_categories[category] = matched_categories
+
 
 
 def sum_occurences_over_time(songs_dict) -> pd.DataFrame:
     columns = [str(i) for i in range(1998, 2024)]
-    index = list(categories.categories.keys())
+    index = list(categories.categories.keys()) + ['number_of_songs'] + \
+            [key + "_normalized" for key, _ in categories.categories.items()]
     results = pd.DataFrame(0,
                            index=index,
                            columns=columns)
     for song in songs_dict.song_list:
         if song.release_date not in [None, "unidentified"]:
             year = song.release_date[0:4]
-            for category, matched_word_list in song.matched_categories.items():
+            if year in columns:
+                results.at['number_of_songs', year] += 1
+            for category, matched_word_dict in song.matched_categories.items():
                 if year in columns:
                     results.at[category, year] = results.at[category, year] + \
-                                                 [sum(count for _, count, _ in matched_word_list)]
+                                                 sum([count for word, count in matched_word_dict.items()])
+
+    for year in columns:
+        for category, _ in categories.categories.items():
+            if results.at['number_of_songs', year] != 0:
+                results.at[category + "_normalized", year] = \
+                    round(float(results.at[category, year]) / float(results.at['number_of_songs', year]), 2)
+            else:
+                results.at[category + "_normalized", year] = 0
+
     return results
