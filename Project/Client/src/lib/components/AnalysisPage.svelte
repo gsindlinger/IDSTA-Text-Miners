d9571c37fb41a571eebbe648128ea0fe9419c3d1
Gabriel Sindlinger <60215105+gsindlinger@users.noreply.github.com>
2023-03-01 20:50:22 +0100

    fix small bugs

--- Project/Client/src/lib/components/AnalysisPage.svelte
+++ Project/Client/src/lib/components/AnalysisPage.svelte
@@ -23,7 +23,7 @@
             }else{
                 return ({
                     value: val,
-                    checked: false
+                    checked: true
                 })
             }
         })
